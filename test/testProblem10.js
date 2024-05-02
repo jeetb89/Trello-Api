4c0dfa3c53bbd008719bdee67397eadb803bce16
Jeetraj Bisht <jeetraj.bisht.28.1@mountblue.tech>
2024-05-02 17:19:17 +0530

    Update all checkitems in a checklist to incomplete status sequentially

---
+++ test/testProblem10.js
@@ -0,0 +1,8 @@
+const cardid = "66307f12636d5befa225cc9c";
+const updateCheckItemsSequentially=require('../problem10.js')
+
+
+updateCheckItemsSequentially(cardid)
+.catch((error)=>{
+    console.log(error)
+})
