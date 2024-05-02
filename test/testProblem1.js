6326c77b5c97ca042162e6c8a0d8c81a7e819607
Jeetraj Bisht <jeetraj.bisht.28.1@mountblue.tech>
2024-05-02 17:16:59 +0530

    takes the boardId  and returns board data

---
+++ test/testProblem1.js
@@ -0,0 +1,11 @@
+const boardId = "66307e57ee94e7290b22a30f";
+
+const getBoard= require('../problem1.js')
+
+  getBoard(boardId)
+  .then((boardData) => {
+    console.log(boardData);
+  })
+  .catch((error) => {
+    console.error("Error fetching board data:", error);
+  });
