47934ed78364d3d5362375a2998980c6deb594fe
Jeetraj Bisht <jeetraj.bisht.28.1@mountblue.tech>
2024-05-03 17:26:09 +0530

    Updated all checkitems in a checklist to completed status simultaneously

--- problem9.js
+++ problem9.js
@@ -1,7 +1,6 @@
 const keyValue = "eae633dae6fa76a149ad9223c1a2c552";
 const tokenValue =
   "ATTA07a64511c9fd8ab2e80abcea1105c3e240d0b1e1cd98104ccf3a0f990634655d0734081C";
-const cardid = "66307f12636d5befa225cc9c";
 
 function createfetch(path, tempMethod, body) {
   return fetch(path, {
@@ -14,7 +13,7 @@ function createfetch(path, tempMethod, body) {
 }
 
 function updateCheckItems(cardId) {
-  createfetch(
+  return createfetch(
     `https://api.trello.com/1/cards/${cardId}/checkLists?key=${keyValue}&token=${tokenValue}`,
     "GET"
   )
@@ -55,8 +54,9 @@ function updateCheckItems(cardId) {
       console.log(results);
     })
     .catch((error) => {
-      console.log("Error:", error);
+      console.log('Error updating check items :',error.message);
+      throw error;
     });
 }
 
-updateCheckItems(cardid);
+module.exports=updateCheckItems;
--- test/testProblem9.js
+++ test/testProblem9.js
@@ -1,4 +1,7 @@
 const cardid = "66307f12636d5befa225cc9c";
 const updateCheckItems=require('../problem9.js')
 
-updateCheckItems(cardid);
+updateCheckItems(cardid)
+.catch((error)=>{
+    console.log("Error Found :",error.message);
+})
